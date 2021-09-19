be1c9f1f5ed942b582e426ca40c3f2fe24c2dc33
Ignas Biekša <ignasbieksa@gmail.com>
2021-09-19 19:40:36 +0300

    finished hashing function

--- main.cpp
+++ main.cpp
@@ -3,17 +3,26 @@
 
 std::string hashFun(std::string str){
 
-    int primeInt = 7103;
+    static const int primeInt = 7103;
     unsigned int hash = primeInt;
     for(int i=0; i<str.length(); ++i){
-        hash = (hash*31) + int(str[i]); // (hash << 5) - hash + int(str[i])
+        hash = (hash*31) + int(str[i]);
     }
 
     std::string hashStr = std::to_string(hash);
     int hashStrSize = hashStr.size();
 
-    std::string hex = "0123456789abcdef";
+    static const std::string hex = "0123456789abcdef";
     int hexSize = hex.size();    
+
+    unsigned int index = hash;
+    std::string hexHash;
+
+    for(int i=0; i<64; i++){
+        index += int(hashStr[index % hashStrSize]);
+        hexHash += hex[index % hexSize];
+    }
+    return hexHash;
 
 }
 
