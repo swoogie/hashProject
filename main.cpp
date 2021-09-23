c72e62f1f1d8752a174449292c1303da4e3f8778
Ignas Biekša <ignasbieksa@gmail.com>
2021-09-23 13:23:51 +0300

    added file reading functionality

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
 
-std::string hashFun(std::string str){
+using std::stringstream;
+using std::ifstream;
+using std::string;
+using std::cout;
+using std::cin;
+
+
+stringstream returnBuff(string fileName){
+    ifstream rd(fileName);
+    try {
+        if(!rd){
+            throw ("Invalid file name");
+        }
+    }
+    catch (string invalidName){
+        cout << invalidName;
+    } 
+
+    stringstream buff;
+    buff << rd.rdbuf();
+
+    return buff;
+}
+
+string read(string fileName){
+    stringstream buff = returnBuff(fileName);
+    string readFile = buff.str();
+    // stringstream().swap(buff); buff.str(""); buff.clear();
+    cout << readFile << std::endl;
+    return readFile;
+}
+
+string hashFun(string str){
 
     static const int primeInt = 7103;
     unsigned int hash = primeInt;
@@ -9,27 +43,46 @@ std::string hashFun(std::string str){
         hash = (hash*31) + int(str[i]);
     }
 
-    std::string hashStr = std::to_string(hash);
+    string hashStr = std::to_string(hash);
     int hashStrSize = hashStr.size();
 
-    static const std::string hex = "0123456789abcdef";
-    int hexSize = hex.size();    
-
     unsigned int index = hash;
-    std::string hexHash;
+    static const string hex = "0123456789abcdef";
+    string hexHash;
 
     for(int i=0; i<64; i++){
-        index += int(hashStr[index % hashStrSize]);
-        hexHash += hex[index % hexSize];
+        index += int(hashStr[i % hashStrSize]) + i*primeInt*31;
+        hexHash += hex[index % 16];
     }
     return hexHash;
 
 }
 
+string indexInput(string input){
+    if(input == "0"){
+        cout << "input: \n";
+        cin.clear();
+        cin.ignore();
+        getline(cin, input);
+        cout << hashFun(input);
+    }
+    else if(input == "1"){
+        cout << "hash of file1.txt: " << hashFun(read("file1.txt")) << "\n";
+        cout << "hash of file2.txt: " << hashFun(read("file2.txt")) << "\n";
+    }
+    else{
+        cout << "enter valid value \n0 - for manual input \n1 - for comparison of 2 files different by 1 character\n";
+        cin >> input;
+        indexInput(input);
+    }
+    return " ";
+}
+
+
 int main(){
-    std::string input;
-    
-    std::cout << "input: \n";
-    std::getline(std::cin, input);
-    std::cout << hashFun(input);
+    string input;
+    cout << "0 - for manual input \n1 - for comparison of 2 files different by 1 character\n";
+    cin >> input;
+
+    indexInput(input);
 }
