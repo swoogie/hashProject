e8bdf252b7d3806e7634fc8873f26f71008fbb5d
Ignas Biekša <ignasbieksa@gmail.com>
2021-09-26 20:05:04 +0300

    added different algorithms for testing, changed rng header file

--- rng.cpp
+++ rng.cpp
@@ -1,18 +1,5 @@
 #include "rng.hpp"
 
-class randChar{
-    public:
-        randChar(int low, int high) :  gen(seed), range(low, high) {}
-        int operator()(){
-            return range(gen);
-        }
-    private:
-        // random_device rd is brokey
-        const unsigned int seed = (std::chrono::system_clock::now().time_since_epoch().count()); 
-        std::mt19937 gen;
-        std::uniform_int_distribution<> range;
-};
-
 void fillFile(std::ofstream &pr){
     for(int i=0; i<1001; i++){
         char charizard = rand();
--- rng.hpp
+++ rng.hpp
@@ -8,7 +8,18 @@
 
 using std::string;
 
-class randChar;
+class randChar{
+    public:
+        randChar(int low, int high) :  gen(seed), range(low, high) {}
+        int operator()(){
+            return range(gen);
+        }
+    private:
+        // random_device rd is brokey
+        const unsigned int seed = (std::chrono::system_clock::now().time_since_epoch().count()); 
+        std::mt19937 gen;
+        std::uniform_int_distribution<> range;
+};
 
 void generateTextFile(string newFileName);
 
