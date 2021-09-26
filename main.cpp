6d2383d491d2d8435501b745a445760705cd7bcc
Ignas Biekša <ignasbieksa@gmail.com>
2021-09-26 20:15:41 +0300

    added inputFiles folder

--- main.cpp
+++ main.cpp
@@ -20,22 +20,22 @@ void indexInput(string input){
         cout << hashFun(input);
     }
     else if(input == "1"){
-        cout << "hash of file1.txt: " << hashFun(read("file1.txt")) << "\n";
-        cout << "hash of file2.txt: " << hashFun(read("file2.txt")) << "\n";
+        cout << "hash of file1.txt: " << hashFun(read("inputFiles/file1.txt")) << "\n";
+        cout << "hash of file2.txt: " << hashFun(read("inputFIles/file2.txt")) << "\n";
     }
     else if(input == "2"){
-        generateTextFile("generatedFile1.txt");
-        generateTextFile("generatedFile2.txt");
+        generateTextFile("inputFiles/generatedFile1.txt");
+        generateTextFile("inputFiles/generatedFile2.txt");
         cout << "files generated. \n";
-        cout << "hash of generatedFile1.txt: " << hashFun(read("generatedFile1.txt")) << "\n";
-        cout << "hash of generatedFile2.txt: " << hashFun(read("generatedFile2.txt")) << "\n";
+        cout << "hash of generatedFile1.txt: " << hashFun(read("inputFiles/generatedFile1.txt")) << "\n";
+        cout << "hash of generatedFile2.txt: " << hashFun(read("inputFiles/generatedFile2.txt")) << "\n";
     }
     else if(input == "3"){
-        cout << "hash of loremA.txt: " << hashFun(read("loremA.txt")) << "\n";
-        cout << "hash of loremB.txt: " << hashFun(read("loremB.txt")) << "\n";
+        cout << "hash of loremA.txt: " << hashFun(read("inputFiles/loremA.txt")) << "\n";
+        cout << "hash of loremB.txt: " << hashFun(read("inputFiles/loremB.txt")) << "\n";
     }
     else if(input == "4"){
-        cout << "hash of empty.txt: " << hashFun(read("empty.txt")) << "\n";
+        cout << "hash of empty.txt: " << hashFun(read("inputFiles/empty.txt")) << "\n";
     }
     else{
         cout << "enter valid value \n0 - for manual input \n1 - for comparison of 2 files w/ 1 different character \n2 - for 2 files with > 1000 randomly generated characters\n";
